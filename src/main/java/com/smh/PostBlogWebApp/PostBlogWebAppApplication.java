9199e585a6392a7947cdff0f46a577ed597f2f8d
SemihBG <buyukgungorsemih@gmail.com>
2021-01-03 22:19:12 +0300

    Initial commit

---
+++ src/main/java/com/smh/PostBlogWebApp/PostBlogWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.smh.PostBlogWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostBlogWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostBlogWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smh/PostBlogWebApp/PostBlogWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smh.PostBlogWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostBlogWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
