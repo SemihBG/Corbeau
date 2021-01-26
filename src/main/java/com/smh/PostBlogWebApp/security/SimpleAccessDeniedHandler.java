1a7dd507f07c4f4d6832cab24d80dc6dfc7aeca1
SemihBG <buyukgungorsemih@gmail.com>
2021-01-26 14:36:35 +0300

    security implementation

---
+++ src/main/java/com/smh/PostBlogWebApp/security/SimpleAccessDeniedHandler.java
@@ -0,0 +1,19 @@
+package com.smh.PostBlogWebApp.security;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SimpleAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+        httpServletResponse.getWriter().print("Access Denied");
+    }
+
+}
