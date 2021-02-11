a1a87db0c9025b5949cc66fd85d8b08269072c98
SemihBG <buyukgungorsemih@gmail.com>
2021-02-11 13:32:49 +0300

    separate security configuration according to profile and customized access denied response

---
+++ src/main/java/com/smh/PostBlogWebApp/configuration/WebSecurityDevelopmentConfiguration.java
@@ -0,0 +1,23 @@
+package com.smh.PostBlogWebApp.configuration;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Profile("development")
+@EnableWebSecurity
+public class WebSecurityDevelopmentConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                .authorizeRequests()
+                    .anyRequest().permitAll();
+    }
+}
