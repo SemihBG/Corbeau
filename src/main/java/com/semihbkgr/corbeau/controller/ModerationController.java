beed95a431f131abd4b455b6a3ea3a6a5ff8e78b
SemihBG <buyukgungorsemih@gmail.com>
2021-07-18 23:57:44 +0300

    Profile controller and view

--- src/main/java/com/semihbkgr/corbeau/controller/ModerationController.java
+++ src/main/java/com/semihbkgr/corbeau/controller/ModerationController.java
@@ -1,18 +1,24 @@
 package com.semihbkgr.corbeau.controller;
 
 import com.semihbkgr.corbeau.service.ModeratorDetailsService;
+import com.semihbkgr.corbeau.service.ModeratorService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import reactor.core.publisher.Mono;
 
 @Controller
 @RequestMapping("/moderation")
+@RequiredArgsConstructor
 public class ModerationController {
+
+    private final ModeratorService moderatorService;
 
     @GetMapping({"", "/", "/panel"})
     public Mono<String> panel(final Model model) {
@@ -34,5 +40,22 @@ public class ModerationController {
         return "login";
     }
 
+    @GetMapping("/profile/{name}")
+    public Mono<String> profile(@PathVariable("name") String name, final Model model) {
+        return Mono.from(moderatorService.findByName(name))
+                .flatMap(moderator -> {
+                    model.addAttribute("moderator", moderator);
+                    return ReactiveSecurityContextHolder.getContext();
+                }).map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(ModeratorDetailsService.ModeratorDetails.class::cast)
+                .map(moderatorDetails -> {
+                    model.addAttribute("me", moderatorDetails.getUsername().equals(name));
+                    return moderatorDetails;
+                })
+                .then(Mono.just("profile"));
+
+    }
 
 }
