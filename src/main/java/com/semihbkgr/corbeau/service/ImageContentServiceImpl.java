14e9a61e58d5da8efc54dd136986306459eb28f7
SemihBG <buyukgungorsemih@gmail.com>
2021-07-24 23:50:44 +0300

    ImageContentService api code review

--- src/main/java/com/semihbkgr/corbeau/service/ImageContentServiceImpl.java
+++ src/main/java/com/semihbkgr/corbeau/service/ImageContentServiceImpl.java
@@ -3,6 +3,7 @@ package com.semihbkgr.corbeau.service;
 import com.semihbkgr.corbeau.model.Image;
 import com.semihbkgr.corbeau.repository.ImageContentRepository;
 import com.semihbkgr.corbeau.util.ParameterUtils;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Service;
@@ -26,29 +27,48 @@ public class ImageContentServiceImpl implements ImageContentService {
                     return imageContentRepository.save(name.concat(".").concat(extension),filePart);
                 })
                 .then(filePartMono)
-                .map(filePart -> {
-                    var size = 0L;
-                    var contentBAOS = new ByteArrayOutputStream();
-                    for (var dataBuffer : dataBufferList) {
-                        var byteCount = dataBuffer.readableByteCount();
-                        size += byteCount;
-                        var content = new byte[byteCount];
-                        dataBuffer.read(content);
-                        contentBAOS.write(content);
-                    }
-                    var img = ImageIO.read(new ByteArrayInputStream(contentBAOS.toByteArray()));
-                    return Mono.just(Image.builder()
-                            .name(name)
-                            .width(img.getWidth())
-                            .height(img.getHeight())
-                            .size(size)
-                            .build());
-                });
+                .flatMap(this::imageFromFilePart);
     }
 
     @Override
     public Mono<Image> update(int id, String name, Mono<FilePart> filePartMono) {
-        return null;
+        return Mono
+                .defer(()->{
+                    var extension= ParameterUtils.extractExtension(filePart.filename());
+                    return imageContentRepository.exists(name.concat(".").concat(extension));
+                })
+                .map(exists->{
+                    if(exists)
+                        return Mono.error()
+                })
+    }
+
+    private Mono<Image> imageFromFilePart(@NonNull FilePart filePart){
+        return filePart
+                .content()
+                .collectList()
+                .flatMap(dataBufferList->{
+                    try{
+                        var size = 0L;
+                        var contentBAOS = new ByteArrayOutputStream();
+                        for (var dataBuffer : dataBufferList) {
+                            var byteCount = dataBuffer.readableByteCount();
+                            size += byteCount;
+                            var content = new byte[byteCount];
+                            dataBuffer.read(content);
+                            contentBAOS.write(content);
+                        }
+                        var img = ImageIO.read(new ByteArrayInputStream(contentBAOS.toByteArray()));
+                        return Mono.just(Image.builder()
+                                .name(name)
+                                .width(img.getWidth())
+                                .height(img.getHeight())
+                                .size(size)
+                                .build());
+                    }catch (Throwable t){
+                        return Mono.error(t);
+                    }
+                });
     }
 
 }
