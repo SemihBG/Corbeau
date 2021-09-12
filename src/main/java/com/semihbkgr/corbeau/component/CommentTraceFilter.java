144b18053cdab36065154724d1b35cc650c1a0a1
SemihBG <buyukgungorsemih@gmail.com>
2021-09-12 23:45:01 +0300

    Comment and Login trace filter class url bean as component and url check

--- src/main/java/com/semihbkgr/corbeau/component/CommentTraceFilter.java
+++ src/main/java/com/semihbkgr/corbeau/component/CommentTraceFilter.java
@@ -2,6 +2,9 @@ package com.semihbkgr.corbeau.component;
 
 import com.semihbkgr.corbeau.service.RequestTraceService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -9,6 +12,8 @@ import reactor.core.publisher.Mono;
 
 import java.net.InetSocketAddress;
 
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CommentTraceFilter implements WebFilter {
 
     private final RequestTraceService commentTraceRepository;
@@ -19,6 +24,8 @@ public class CommentTraceFilter implements WebFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange serverWebExchange, WebFilterChain webFilterChain) {
+        if(!serverWebExchange.getRequest().getURI().toString().startsWith("/moderation/login"))
+            return webFilterChain.filter(serverWebExchange);
         InetSocketAddress clientRemoteAddress = serverWebExchange.getRequest().getRemoteAddress();
         if (clientRemoteAddress == null)
             return Mono.error(new IllegalStateException("Client remote address is null"));
@@ -26,9 +33,6 @@ public class CommentTraceFilter implements WebFilter {
         if (clientIpAddr == null)
             return Mono.error(new IllegalStateException("Client ip address is null"));
         var clientRequest = commentTraceRepository.increaseAndUpdate(clientIpAddr);
-        System.out.println("-----------------");
-        System.out.println(clientRequest);
-        System.out.println("-----------------");
         if (clientRequest.getRequestCount() < 5)
             return webFilterChain.filter(serverWebExchange);
         else return Mono.empty();
--- src/main/java/com/semihbkgr/corbeau/component/LoginTraceFilter.java
+++ src/main/java/com/semihbkgr/corbeau/component/LoginTraceFilter.java
@@ -2,6 +2,9 @@ package com.semihbkgr.corbeau.component;
 
 import com.semihbkgr.corbeau.service.RequestTraceService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -9,6 +12,8 @@ import reactor.core.publisher.Mono;
 
 import java.net.InetSocketAddress;
 
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class LoginTraceFilter implements WebFilter {
 
     private final RequestTraceService loginTraceRepository;
@@ -19,6 +24,8 @@ public class LoginTraceFilter implements WebFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange serverWebExchange, WebFilterChain webFilterChain) {
+        if(!serverWebExchange.getRequest().getURI().toString().startsWith("/api/commnet"))
+            return webFilterChain.filter(serverWebExchange);
         InetSocketAddress clientRemoteAddress = serverWebExchange.getRequest().getRemoteAddress();
         if (clientRemoteAddress == null)
             return Mono.error(new IllegalStateException("Client remote address is null"));
