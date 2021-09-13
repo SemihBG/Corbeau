72a922ccb6b91bccbd59b1a648cc8c20f6d26c01
SemihBG <buyukgungorsemih@gmail.com>
2021-09-13 21:31:54 +0300

    GlobalErrorAttributes and GlobalErrorWebExceptionHandler classes, error properties as method and exceptionObject

---
+++ src/main/java/com/semihbkgr/corbeau/component/GlobalErrorWebExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.semihbkgr.corbeau.component;
+
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Mono;
+
+@Component
+@Order(-2)
+public class GlobalErrorWebExceptionHandler extends AbstractErrorWebExceptionHandler {
+
+    public GlobalErrorWebExceptionHandler(ErrorAttributes errorAttributes, WebProperties.Resources resources,
+                                          ApplicationContext applicationContext, ServerCodecConfigurer serverCodecConfigurer) {
+        super(errorAttributes, resources, applicationContext);
+        setMessageWriters(serverCodecConfigurer.getWriters());
+    }
+
+    @Override
+    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
+        return RouterFunctions.route(RequestPredicates.all(), this::errorServerResponse);
+    }
+
+    private Mono<ServerResponse> errorServerResponse(ServerRequest request) {
+        var errorPropertiesMap = super.getErrorAttributes(request,
+                ErrorAttributeOptions.of(ErrorAttributeOptions.Include.EXCEPTION, ErrorAttributeOptions.Include.MESSAGE));
+        return ServerResponse
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(errorPropertiesMap));
+    }
+
+}
