d354a7a4bb537c02172e9068f54558dd964419af
SemihBG <buyukgungorsemih@gmail.com>
2021-09-19 19:54:48 +0300

    menu.html update design and new colors, menu.js shuffle tags

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,11 +1,15 @@
 function initialize(){
     var tagList=document.getElementsByClassName("tag-list")[0]
     var tags=tagList.getElementsByTagName("li")
+    var ul = document.getElementById("tags")
+    for (var i = ul.children.length; i >= 0; i--) {
+        ul.appendChild(ul.children[Math.random() * i | 0]);
+    }
     for (let i = 0; i < tags.length; i++) {
-        tags[i].style.marginRight=(Math.floor(Math.random() * 60) + 20)+"px";
-        tags[i].style.marginLeft=(Math.floor(Math.random() * 50))+"px";
-        tags[i].style.marginTop=(Math.floor(Math.random() * 10) + 10)+"px";
-        tags[i].style.marginBottom=(Math.floor(Math.random() * 10))+"px";
+        tags[i].style.marginRight=(Math.floor(Math.random() * 63) + 23)+"px";
+        tags[i].style.marginLeft=(Math.floor(Math.random() * 53))+"px";
+        tags[i].style.marginTop=(Math.floor(Math.random() * 11) + 11)+"px";
+        tags[i].style.marginBottom=(Math.floor(Math.random() * 11))+"px";
     }
 }
 
